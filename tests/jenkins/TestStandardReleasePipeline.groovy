3c28f302d259fecabec00474973b08528eab1fcb
Sayali Gaikawad <gaiksaya@amazon.com>
2022-10-19 12:19:08 -0700

    Add more tests
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- tests/jenkins/TestStandardReleasePipeline.groovy
+++ tests/jenkins/TestStandardReleasePipeline.groovy
@@ -10,18 +10,58 @@
 import jenkins.tests.BuildPipelineTest
 import org.junit.Before
 import org.junit.Test
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.CoreMatchers.equalTo
+import static com.lesfurets.jenkins.unit.MethodCall.callArgsToString
+import static org.hamcrest.CoreMatchers.hasItem
+
 
 
 class TestStandardReleasePipeline extends BuildPipelineTest {
 
     @Before
     void setUp() {
-        this.registerLibTester(new StandardReleasePipelineLibTester('AL2-X64', 'test:image'))
         super.setUp()
     }
 
     @Test
     void testStandardReleasePipeline() {
-        super.testPipeline("tests/jenkins/jobs/StandardReleasePipeline_JenkinsFile")
+        super.testPipeline("tests/jenkins/jobs/StandardReleasePipeline_Jenkinsfile")
+    }
+
+    @Test
+    void testStandardReleasePipelineWithArgs() {
+        super.testPipeline("tests/jenkins/jobs/StandardReleasePipelineWithArgs_JenkinsFile")
+    }
+
+    @Test
+    void 'check overide values'() {
+        runScript('tests/jenkins/jobs/StandardReleasePipelineWithArgs_JenkinsFile')
+        def echoCommand = getEchoCommands().findAll{
+            command -> command.contains('agent')
+        }
+
+        assertThat(echoCommand.size(), equalTo(1))
+        assertThat(echoCommand, hasItem('Executing on agent [docker:[image:test:image, reuseNode:false, stages:[:], args:, alwaysPull:true, containerPerStageRoot:false, label:AL2-X64]]'))
+    }
+
+    @Test 
+    void 'check default values'(){
+        runScript('tests/jenkins/jobs/StandardReleasePipeline_Jenkinsfile')
+        def echoCommand = getEchoCommands().findAll{
+            command -> command.contains('agent')
+        }
+
+        assertThat(echoCommand.size(), equalTo(1))
+        assertThat(echoCommand, hasItem('Executing on agent [docker:[image:opensearchstaging/ci-runner:ci-runner-centos7-opensearch-build-v2, reuseNode:false, stages:[:], args:, alwaysPull:true, containerPerStageRoot:false, label:Jenkins-Agent-AL2-X64-C54xlarge-Docker-Host]]'))
+    }
+
+    def getEchoCommands() {
+        def echoCommands = helper.callStack.findAll { call ->
+            call.methodName == 'echo'
+        }.collect { call ->
+            callArgsToString(call)
+        }
+        return echoCommands
     }
 }
