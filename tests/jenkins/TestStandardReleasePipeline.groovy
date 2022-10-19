adc233816626a0e97fe679a57af9c39e4f7c391d
Sayali Gaikawad <61760125+gaiksaya@users.noreply.github.com>
2022-10-19 15:52:41 -0700

    Add standard release pipeline library (#11)
    * Add standard release pipeline library
    
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

---
+++ tests/jenkins/TestStandardReleasePipeline.groovy
@@ -0,0 +1,68 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+
+import jenkins.tests.BuildPipelineTest
+import org.junit.Before
+import org.junit.Test
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.CoreMatchers.equalTo
+import static com.lesfurets.jenkins.unit.MethodCall.callArgsToString
+import static org.hamcrest.CoreMatchers.hasItem
+
+
+
+class TestStandardReleasePipeline extends BuildPipelineTest {
+
+    @Before
+    void setUp() {
+        super.setUp()
+    }
+
+    @Test
+    void testStandardReleasePipeline() {
+        super.testPipeline('tests/jenkins/jobs/StandardReleasePipeline_JenkinsFile')
+    }
+
+    @Test
+    void testStandardReleasePipelineWithArgs() {
+        super.testPipeline('tests/jenkins/jobs/StandardReleasePipelineWithArgs_JenkinsFile')
+    }
+
+    @Test
+    void 'check override values'() {
+        runScript("tests/jenkins/jobs/StandardReleasePipelineWithArgs_JenkinsFile")
+        def echoCommand = getEchoCommands().findAll{
+            command -> command.contains('agent')
+        }
+
+        assertThat(echoCommand.size(), equalTo(1))
+        assertThat(echoCommand, hasItem('Executing on agent [docker:[image:test:image, reuseNode:false, stages:[:], args:, alwaysPull:true, containerPerStageRoot:false, label:AL2-X64]]'))
+    }
+
+    @Test 
+    void 'check default values'(){
+        runScript("tests/jenkins/jobs/StandardReleasePipeline_JenkinsFile")
+        def echoCommand = getEchoCommands().findAll{
+            command -> command.contains('agent')
+        }
+
+        assertThat(echoCommand.size(), equalTo(1))
+        assertThat(echoCommand, hasItem('Executing on agent [docker:[image:opensearchstaging/ci-runner:release-centos7-clients-v1, reuseNode:false, stages:[:], args:, alwaysPull:true, containerPerStageRoot:false, label:Jenkins-Agent-AL2-X64-C54xlarge-Docker-Host]]'))
+    }
+
+    def getEchoCommands() {
+        def echoCommands = helper.callStack.findAll { call ->
+            call.methodName == 'echo'
+        }.collect { call ->
+            callArgsToString(call)
+        }
+        return echoCommands
+    }
+}
---
+++ vars/standardReleasePipeline.groovy
@@ -0,0 +1,48 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+/** A standard release pipeline for OpenSearch projects
+@param Map args = [:] args A map of the following parameters
+@param body <Required> - A closure containing release steps to be executed in release stage.
+@param args.overrideAgent <Optional> - Jenkins agent label to override the default.
+@param args.overrideDockerImage <Optional> - Docker image to override the default.
+*/
+
+void call(Map args = [:], Closure body) {
+    pipeline {
+        agent
+        {
+            docker {
+                label args.overrideAgent ?: 'Jenkins-Agent-AL2-X64-C54xlarge-Docker-Host'
+                image args.overrideDockerImage ?: 'opensearchstaging/ci-runner:release-centos7-clients-v1'
+                alwaysPull true
+            }
+        }
+        options {
+            timeout(time: 1, unit: 'HOURS')
+        }
+        stages{
+            stage("Release") {
+                steps {
+                    script {
+                        body()
+                    }
+                }
+            }
+        }
+        post {
+            always {
+                script {
+                    postCleanup()
+                    sh 'docker image prune -f --all'
+                    }
+                }
+            }
+        }
+    }
