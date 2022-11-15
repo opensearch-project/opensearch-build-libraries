24c3bf10dbe15596d35f012f4c7c307b3c023fea
Sayali Gaikawad <gaiksaya@amazon.com>
2022-11-14 19:28:05 -0800

    Add library to publish artifacts to PyPi
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- vars/publishToPyPi.groovy
+++ vars/publishToPyPi.groovy
@@ -9,23 +9,19 @@
 
 /** Library to publish artifacts to PyPi registry with OpenSearch as maintainer
 @param Map args = [:] args A map of the following parameters
-@param args.repository <required> - Repository to be used to publish the artifact to npm
-@param args.tag <required> - Tag reference to be used to publish the artifact
-@param args.dir <optional> - The directory containing distribution files to upload to the repository. Defaults to 'dist/*'
-@param args.signArtifacts <optional> - Sign the artifacts to be uploaded to PyPi. Defaults to 'true'
+@param args.artifactsPath <optional> - The directory containing distribution files to upload to the repository. Defaults to 'dist/*'
 */
 void call(Map args = [:]) {
-    if (args.signArtifacts) {
-        lib = library(identifier: 'jenkins@pypi', retriever: legacySCM(scm))
-        signArtifacts(
-            artifactPath: args.dir,
-            sigtype: '.asc',
-            platform: 'linux'
-        )
-    }
+    lib = library(identifier: 'jenkins@main', retriever: legacySCM(scm))
+    String releaseArtifactsDir = args.artifactsPath ?: 'dist'
+
+    signArtifacts(
+        artifactPath: releaseArtifactsDir,
+        sigtype: '.asc',
+        platform: 'linux'
+    )
 
     withCredentials([usernamePassword(credentialsId: 'jenkins-opensearch-pypi-username', usernameVariable: 'TWINE_USERNAME', passwordVariable: 'TWINE_PASSWORD')]) {
-            def dist = args.dir ?: 'dist/*'
-            sh """twine upload -r pypi ${dist}"""
-        }
+            sh """twine upload -r pypi ${releaseArtifactsDir}"""
+    }
 }
