ccb67ac62e3e249d478a98571e0fdf6fcad8942e
Sayali Gaikawad <61760125+gaiksaya@users.noreply.github.com>
2022-11-28 15:33:21 -0800

    Add publishToRubyGems library (#49)
    * Add publishToRubyGems library
    
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

---
+++ vars/publishToRubyGems.groovy
@@ -0,0 +1,26 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+/** Library to publish ruby gems to rubygems.org registry with OpenSearch as the owner.
+Note: Please make sure the gem is already signed.
+@param Map args = [:] args A map of the following parameters
+@param args.apiKeyCredentialId <required> - Credential id consisting api key for publishing the gem to rubyGems.org
+@param args.gemsDir <optional> - The directory containing the gem to be published. Defaults to 'dist'
+@params args.publicCertPath <optional> - The relative path to public key. Defaults to 'cert/opensearch-rubygems.pem'
+*/
+void call(Map args = [:]) {
+    String releaseArtifactsDir = args.gemsDir ? "${WORKSPACE}/${args.gemsDir}" : "${WORKSPACE}/dist"
+    String certPath = args.publicCertPath ? "${WORKSPACE}/${args.publicCertPath}" : "${WORKSPACE}/cert/opensearch-rubygems.pem"
+
+    withCredentials([string(credentialsId: "${args.apiKeyCredentialId}", variable: 'API_KEY')]) {
+        sh """gem cert --add ${certPath} && \
+            cd ${releaseArtifactsDir} && gem install `ls *.gem` -P HighSecurity && \
+            curl --fail --data-binary @`ls *.gem` -H 'Authorization:${API_KEY}' -H 'Content-Type: application/octet-stream' https://rubygems.org/api/v1/gems"""
+        }
+}
