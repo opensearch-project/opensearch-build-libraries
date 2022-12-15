52a27d3e68ad68cb86eb0563aaeea67620826d1d
Sayali Gaikawad <61760125+gaiksaya@users.noreply.github.com>
2022-12-14 18:41:43 -0800

    Update version to 1.5.1 (#101)
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- vars/rpmDistValidation.groovy
+++ vars/rpmDistValidation.groovy
@@ -13,7 +13,7 @@
  */
 def call(Map args = [:]) {
 
-    def lib = library(identifier: 'jenkins@1.5.0', retriever: legacySCM(scm))
+    def lib = library(identifier: 'jenkins@1.5.1', retriever: legacySCM(scm))
     def bundleManifestURL = args.bundleManifestURL
     sh ("curl -sL $bundleManifestURL -o $WORKSPACE/manifest.yml")
     def bundleManifest = "$WORKSPACE/manifest.yml"
