c66bc1c3060b8154f86b0fdf335fec8049421b42
Sayali Gaikawad <61760125+gaiksaya@users.noreply.github.com>
2023-02-01 18:17:53 -0800

    Support switching ruby gems version and fix regex (#119)
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- vars/publishToRubyGems.groovy
+++ vars/publishToRubyGems.groovy
@@ -8,23 +8,26 @@
  */
 
 /** Library to publish ruby gems to rubygems.org registry with OpenSearch as the owner.
-Note: Please make sure the gem is already signed.
+Note: Please make sure the gem is already signed. For ruby versions, they to be preinstalled as a part of docker. See the default docker file: https://github.com/opensearch-project/opensearch-build/blob/main/docker/ci/dockerfiles/current/release.centos.clients.x64.arm64.dockerfile
 @param Map args = [:] args A map of the following parameters
 @param args.apiKeyCredentialId <required> - Credential id consisting api key for publishing the gem to rubyGems.org
 @param args.gemsDir <optional> - The directory containing the gem to be published. Defaults to 'dist'
 @params args.publicCertPath <optional> - The relative path to public key. Defaults to 'certs/opensearch-rubygems.pem'
+@params args.rubyVersion <optional> - Ruby version to be used. Defaults to 2.6.0
 */
 
 
 void call(Map args = [:]) {
     String releaseArtifactsDir = args.gemsDir ? "${WORKSPACE}/${args.gemsDir}" : "${WORKSPACE}/dist"
     String certPath = args.publicCertPath ? "${WORKSPACE}/${args.publicCertPath}" : "${WORKSPACE}/certs/opensearch-rubygems.pem"
+    String rubyVersion = args.rubyVersion ?: '2.6.0'
 
-    sh """
+    sh """#!/bin/bash
         gem cert --add ${certPath}
+        source /usr/share/opensearch/.rvm/scripts/rvm && rvm use ${rubyVersion} && ruby --version
         cd ${releaseArtifactsDir} && gemNameWithVersion=\$(ls *.gem)
         gem install \$gemNameWithVersion
-        gemName=\$(echo \$gemNameWithVersion | sed -E 's/(-[0-9.]+.gem\$)//g')
+        gemName=\$(echo \$gemNameWithVersion | sed -E 's/(-[0-9.]+-*[a-z]*.gem\$)//g')
         gem uninstall \$gemName
         gem install \$gemNameWithVersion -P HighSecurity
     """
