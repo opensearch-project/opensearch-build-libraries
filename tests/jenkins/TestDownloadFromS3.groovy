462aec79bdd3521146e307fdcd80c615206770d9
Sayali Gaikawad <61760125+gaiksaya@users.noreply.github.com>
2023-02-09 17:31:38 -0800

    Enhance downloadFromS3 library (#135)
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- tests/jenkins/TestDownloadFromS3.groovy
+++ tests/jenkins/TestDownloadFromS3.groovy
@@ -10,6 +10,9 @@
 import jenkins.tests.BuildPipelineTest
 import org.junit.Before
 import org.junit.Test
+import static org.hamcrest.CoreMatchers.hasItem
+import static org.hamcrest.MatcherAssert.assertThat
+import static com.lesfurets.jenkins.unit.MethodCall.callArgsToString
 
 
 class TestDownloadFromS3 extends BuildPipelineTest {
@@ -17,13 +20,55 @@ class TestDownloadFromS3 extends BuildPipelineTest {
     @Before
     void setUp() {
 
-        this.registerLibTester(new DownloadFromS3LibTester('/tmp/src/path' , 'dummy_bucket', '/download/path', true))
+        this.registerLibTester(new DownloadFromS3LibTester(
+            'tmp-role',
+            'role-credential-id',
+            '/download/path',
+            'dummy_bucket',
+            "/tmp"
+            ))
+        this.registerLibTester(new DownloadFromS3LibTester(
+            'tmp-role',
+            'role-credential-id',
+            '/download/path',
+            'dummy_bucket',
+            "/tmp",
+            false,
+            'us-west-2'
+            ))
 
         super.setUp()
     }
 
     @Test
     public void testDownloadFromS3() {
-        super.testPipeline("tests/jenkins/jobs/DownloadFromS3_Jenkinsfile")
+        super.testPipeline('tests/jenkins/jobs/DownloadFromS3_Jenkinsfile')
+    }
+
+    @Test
+    void verify_default_args(){
+        runScript('tests/jenkins/jobs/DownloadFromS3_Jenkinsfile')
+        def aws = getMethodCall('withAWS')
+        assertThat(aws, hasItem('{role=tmp-role, roleAccount=AWS_ACCOUNT_NUMBER, duration=900, roleSessionName=jenkins-session, region=us-east-1}, groovy.lang.Closure'))
+        def download = getMethodCall('s3Download')
+        assertThat(download, hasItem('{file=/tmp, bucket=dummy_bucket, path=/download/path, force=false}'))
+    }
+
+    @Test
+    void verify_optional_args(){
+        runScript('tests/jenkins/jobs/DownloadFromS3_Jenkinsfile')
+        def aws = getMethodCall('withAWS')
+        assertThat(aws, hasItem('{role=tmp-role, roleAccount=AWS_ACCOUNT_NUMBER, duration=900, roleSessionName=jenkins-session, region=us-west-2}, groovy.lang.Closure'))
+        def download = getMethodCall('s3Download')
+        assertThat(download, hasItem('{file=/tmp, bucket=dummy_bucket, path=/download/path, force=true}'))
+    }
+
+    def getMethodCall(methodName) {
+        def shCommands = helper.callStack.findAll { call ->
+            call.methodName == methodName
+        }.collect { call ->
+            callArgsToString(call)
+        }
+        return shCommands
     }
 }
--- vars/downloadFromS3.groovy
+++ vars/downloadFromS3.groovy
@@ -6,10 +6,23 @@
  * this file be licensed under the Apache-2.0 license or a
  * compatible open source license.
  */
+ /**
+Library to download the artifacts from S3 bucket to local
+@param Map[assumedRoleName] <Required> - Role to be assumed to download artifacts.
+@param Map[roleAccountNumberCred] <Required> - AWS account number to download artifacts from.
+@param Map[bucketName] <Required> -S3 bucket Name.
+@param Map[downloadPath] <Required> - This is the path inside the bucket to use.
+@param Map[localPath] <Required> - This is the local target file to download into.
+@param Map[region] <Optional> - AWS region of the S3 bucket. Defaults to us-east-1
+@param Map[force]<Optional> - Set this to true to overwrite local workspace files. Defaults to false
+*/
 void call(Map args = [:]) {
-    withCredentials([string(credentialsId: 'jenkins-aws-account-public', variable: 'AWS_ACCOUNT_PUBLIC')]) {
-            withAWS(role: 'opensearch-bundle', roleAccount: "${AWS_ACCOUNT_PUBLIC}", duration: 900, roleSessionName: 'jenkins-session') {
-                s3Download(file: args.destPath, bucket: args.bucket, path: args.path, force: args.force)
+    boolean forceDownload = args.force ?: false
+    String region = args.region ?: 'us-east-1'
+
+    withCredentials([string(credentialsId: "${args.roleAccountNumberCred}", variable: 'AWS_ACCOUNT_NUMBER')]) {
+            withAWS(role: args.assumedRoleName, roleAccount: "${AWS_ACCOUNT_NUMBER}", duration: 900, roleSessionName: 'jenkins-session', region: "${region}") {
+                s3Download(file: args.localPath, bucket: args.bucketName, path: args.downloadPath, force: "${forceDownload}")
             }
     }
 }
