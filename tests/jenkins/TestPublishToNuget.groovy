633849b63e9f94420186f7d485c198a8a339112b
Sayali Gaikawad <gaiksaya@amazon.com>
2023-03-14 17:32:13 -0700

    Fix publishing to Nuget syntax and refactor find
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- tests/jenkins/TestPublishToNuget.groovy
+++ tests/jenkins/TestPublishToNuget.groovy
@@ -36,22 +36,22 @@ class TestPublishToNuget extends BuildPipelineTest {
     }
 
     @Test
-    void 'verify build command'(){
+    void verify_build_command(){
         runScript('tests/jenkins/jobs/PublishToNuget_Jenkinsfile')
 
-        def buildCommands = getShellCommands('build')
-        assertThat(buildCommands, hasItem('\n    dotnet build /tmp/workspace/test-solution-file.sln --configuration Release\n    find src -name OpenSearch*.dll>/tmp/workspace/dlls.txt\n    '))
+        def buildCommand = getShellCommands('dotnet build')
+        assertThat(buildCommand, hasItem('\n    dotnet build /tmp/workspace/test-solution-file.sln --configuration Release\n    find src/OpenSearch.*/bin/Release/*/*.dll -type f -regextype posix-extended -regex \"src/([^/]+)/bin/Release/[^/]+/\\1\\.dll\">/tmp/workspace/dlls.txt\n    '))
     }
 
     @Test
-    void 'verify_pack_command'(){
+    void verify_pack_command(){
         runScript('tests/jenkins/jobs/PublishToNuget_Jenkinsfile')
         def packCommand = getShellCommands('pack')
-        assertThat(packCommand, hasItem('\n            dotnet pack /tmp/workspace/test-solution-file.sln --configuration Release --no-build\n            for package in `find src -name OpenSearch*.nupkg`\n                do\n                    dotnet nuget push $package --api-key API_KEY --source https://api.nuget.org/v3/index.json\n                done\n        '))
+        assertThat(packCommand, hasItem('\n        dotnet pack /tmp/workspace/test-solution-file.sln --configuration Release --no-build\n        find src -name OpenSearch*.nupkg > /tmp/workspace/nupkg.txt\n    '))
     }
 
     @Test
-    void 'verify_signer_call'(){
+    void verify_signer_call(){
         runScript('tests/jenkins/jobs/PublishToNuget_Jenkinsfile')
         def signcommand = getShellCommands('sign.sh')
         assertThat(signcommand, hasItems('\n                   #!/bin/bash\n                   set +x\n                   export ROLE=SIGNER_WINDOWS_ROLE\n                   export EXTERNAL_ID=SIGNER_WINDOWS_EXTERNAL_ID\n                   export UNSIGNED_BUCKET=SIGNER_WINDOWS_UNSIGNED_BUCKET\n                   export SIGNED_BUCKET=SIGNER_WINDOWS_SIGNED_BUCKET\n                   export PROFILE_IDENTIFIER=SIGNER_WINDOWS_PROFILE_IDENTIFIER\n                   export PLATFORM_IDENTIFIER=SIGNER_WINDOWS_PLATFORM_IDENTIFIER\n\n                   /tmp/workspace/opensearch-build/sign.sh /tmp/workspace/one.dll --platform windows --overwrite \n               ',
@@ -59,6 +59,15 @@ class TestPublishToNuget extends BuildPipelineTest {
         '\n                   #!/bin/bash\n                   set +x\n                   export ROLE=SIGNER_WINDOWS_ROLE\n                   export EXTERNAL_ID=SIGNER_WINDOWS_EXTERNAL_ID\n                   export UNSIGNED_BUCKET=SIGNER_WINDOWS_UNSIGNED_BUCKET\n                   export SIGNED_BUCKET=SIGNER_WINDOWS_SIGNED_BUCKET\n                   export PROFILE_IDENTIFIER=SIGNER_WINDOWS_PROFILE_IDENTIFIER\n                   export PLATFORM_IDENTIFIER=SIGNER_WINDOWS_PLATFORM_IDENTIFIER\n\n                   /tmp/workspace/opensearch-build/sign.sh /tmp/workspace/three.dll --platform windows --overwrite \n               '))
     }
 
+    @Test
+    void verify_push_command(){
+        runScript('tests/jenkins/jobs/PublishToNuget_Jenkinsfile')
+        def pushCommand = getShellCommands('push')
+        assertThat(pushCommand, hasItems(
+            'dotnet nuget push /tmp/workspace/src/net/one.nupkg --api-key API_KEY --source https://api.nuget.org/v3/index.json',
+            'dotnet nuget push /tmp/workspace/src/net/two.nupkg --api-key API_KEY --source https://api.nuget.org/v3/index.json',
+            'dotnet nuget push /tmp/workspace/src/net/three.nupkg --api-key API_KEY --source https://api.nuget.org/v3/index.json'))
+    }
     def getShellCommands(searchString) {
         def shCommands = helper.callStack.findAll { call ->
             call.methodName == 'sh'
