243d17f541d54a1fb47fc416c200bfbac1538f0c
Peter Zhu <zhujiaxi@amazon.com>
2023-04-25 20:29:23 -0400

    Add tests
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- tests/jenkins/TestRunIntegTestScript.groovy
+++ tests/jenkins/TestRunIntegTestScript.groovy
@@ -82,14 +82,14 @@ class TestRunIntegTestScript extends BuildPipelineTest {
     @Test
     void 'IntegTest LocalPath SwitchNonRoot=false'() {
         runScript("tests/jenkins/jobs/RunIntegTestScript_LocalPath_Jenkinsfile")
-        assertThat(getShellCommands('sh', 'test.sh'), hasItems('  ./test.sh integ-test tests/data/opensearch-1.3.0-test.yml --component OpenSearch --test-run-id null --paths opensearch=tests/jenkins/artifacts/tar '))
+        assertThat(getShellCommands('sh', 'test.sh'), hasItems('env  ./test.sh integ-test tests/data/opensearch-1.3.0-test.yml --component OpenSearch --test-run-id null --paths opensearch=tests/jenkins/artifacts/tar '))
 
     }
 
     @Test
     void 'IntegTest LocalPath SwitchNonRoot=true'() {
         runScript("tests/jenkins/jobs/RunIntegTestScript_LocalPath_Switch_Non_Root_Jenkinsfile")
-        assertThat(getShellCommands('sh', 'test.sh'), hasItems('su `id -un 1000` -c \"  ./test.sh integ-test tests/data/opensearch-1.3.0-test.yml --component OpenSearch --test-run-id null --paths opensearch=tests/jenkins/artifacts/tar \"'))
+        assertThat(getShellCommands('sh', 'test.sh'), hasItems('su `id -un 1000` -c \"env PATH=$PATH  ./test.sh integ-test tests/data/opensearch-1.3.0-test.yml --component OpenSearch --test-run-id null --paths opensearch=tests/jenkins/artifacts/tar \"'))
 
     }
 
