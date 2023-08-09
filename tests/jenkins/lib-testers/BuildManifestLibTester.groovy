79ee63d7ac90681d18766af6a39ca4f935778431
Sayali Gaikawad <gaiksaya@amazon.com>
2023-08-09 15:12:09 -0700

    Add support for continue-on-error flag for build and add tests cases
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

---
+++ tests/jenkins/lib-testers/BuildManifestLibTester.groovy
@@ -0,0 +1,72 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+import static org.hamcrest.CoreMatchers.notNullValue
+import static org.hamcrest.CoreMatchers.nullValue
+import static org.hamcrest.MatcherAssert.assertThat
+
+class BuildManifestLibTester extends LibFunctionTester {
+    private String inputManifestPath
+    private String distribution = 'tar'
+    private String componentName
+    private Boolean snapshot = false
+    private Boolean lock = false
+    private Boolean continueOnError = false
+
+    public BuildManifestLibTester(String inputManifestPath){
+        this.inputManifestPath = inputManifestPath
+    }
+
+    public BuildManifestLibTester(String inputManifestPath, String distribution, Boolean snapshot){
+        this.inputManifestPath = inputManifestPath
+        this.distribution = distribution
+        this.snapshot = snapshot
+    }
+
+    public BuildManifestLibTester(String inputManifestPath, String distribution, String componentName, Boolean snapshot){
+        this.inputManifestPath = inputManifestPath
+        this.distribution = distribution
+        this.componentName = componentName
+        this.snapshot = snapshot
+    }
+
+    public BuildManifestLibTester(String inputManifestPath, String distribution, Boolean snapshot, Boolean continueOnError){
+        this.inputManifestPath = inputManifestPath
+        this.distribution = distribution
+        this.snapshot = snapshot
+        this.continueOnError = continueOnError
+    }
+
+    public BuildManifestLibTester(String inputManifestPath, String distribution, String componentName, Boolean lock, Boolean continueOnError){
+        this.inputManifestPath = inputManifestPath
+        this.distribution = distribution
+        this.componentName = componentName
+        this.lock = lock
+        this.continueOnError = continueOnError
+    }
+
+    @Override
+    void configure(helper, binding) {
+        helper.registerAllowedMethod("checkout", [Map], {})
+    }
+
+    @Override
+    void parameterInvariantsAssertions(call) {
+        assertThat(call.args.inputManifest.first(), notNullValue())
+    }
+
+    @Override
+    boolean expectedParametersMatcher(call) {
+        return call.args.inputManifest.first().equals(this.inputManifestPath)
+    }
+
+    @Override
+    String libFunctionName() {
+    return 'buildManifest'
+    }
+}
--- vars/buildManifest.groovy
+++ vars/buildManifest.groovy
@@ -6,6 +6,17 @@
  * this file be licensed under the Apache-2.0 license or a
  * compatible open source license.
  */
+ /** Library to call build.sh script (builds OpenSearch and OpenSearch Dashboards distribution)
+ @param Map args = [:] args A map of the following parameters
+ @param args.inputManifest <required> - Relative path to input manifest containing all the components to build.
+ @param args.distribution <optional> - Type of distribution to build. Defaults to null.
+ @param args.componentName <optional> - Name of the single component to build. Defaults to null and builds all the components in the manifest.
+ @param args.platform <optional> - Platform to build. Defaults to null.
+ @param args.architecture <optional> - Architecture to build. Defaults to null.
+ @param args.snapshot <optional> - Boolean value. Defaults to null.
+ @param args.lock <optional> - Generate a stable reference manifest. Defaults to null.
+ @param args.continueOnError <optional> - Do not fail the distribution build on any plugin component failure. Defaults to null
+ */
 void call(Map args = [:]) {
     sh(([
         './build.sh',
@@ -15,6 +26,7 @@ void call(Map args = [:]) {
         args.platform ? "-p ${args.platform}" : null,
         args.architecture ? "-a ${args.architecture}" : null,
         args.snapshot ? '--snapshot' : null,
-        args.lock ? '--lock' : null
+        args.lock ? '--lock' : null,
+        args.continueOnError ? '--continue-on-error' : null,
     ] - null).join(' '))
 }
