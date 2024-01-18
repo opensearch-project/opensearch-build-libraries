8b7df1261220a27f90f1ac7042f8c5256540597c
Zelin Hao <zelinhao@amazon.com>
2024-01-18 11:38:50 -0800

    Add a new groovy library to download the previous build artifacts (#362)
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- vars/buildManifest.groovy
+++ vars/buildManifest.groovy
@@ -16,8 +16,16 @@
  @param args.snapshot <optional> - Boolean value. Defaults to null.
  @param args.lock <optional> - Generate a stable reference manifest. Defaults to null.
  @param args.continueOnError <optional> - Do not fail the distribution build on any plugin component failure. Defaults to null
+ @param args.incremental <optional> - Boolean value to enable incremental build.
  */
 void call(Map args = [:]) {
+    boolean incremental_enabled = args.incremental != null && args.incremental
+
+    if (incremental_enabled) {
+        echo("Incremental build enabled! Retrieving previous build library.")
+        retrievePreviousBuild(args)
+    }
+
     sh(([
         './build.sh',
         args.inputManifest ?: "manifests/${INPUT_MANIFEST}",
@@ -28,5 +36,6 @@ void call(Map args = [:]) {
         args.snapshot ? '--snapshot' : null,
         args.lock ? '--lock' : null,
         args.continueOnError ? '--continue-on-error' : null,
+        incremental_enabled ? '--incremental' : null
     ] - null).join(' '))
 }
---
+++ vars/retrievePreviousBuild.groovy
@@ -0,0 +1,60 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+/** Library to download previous build artifacts into the workspace.
+ @param Map args = [:] args A map of the following parameters
+ @param args.inputManifest <required> - Relative path to input manifest containing all the components to build.
+ @param args.jobName <optional> - Relative path to input manifest containing all the components to build.
+ @param args.platform <required> - Platform of previous build to retrieve.
+ @param args.architecture <required> - Architecture of previous build to retrieve.
+ @param args.distribution <required> - Distribution of previous build to retrieve.
+ @param args.previousBuildId <optional> - Build id of previous build for incremental build. Defaults to latest.
+ */
+void call(Map args = [:]) {
+    def lib = library(identifier: 'jenkins@main', retriever: legacySCM(scm))
+    def inputManifestObj = lib.jenkins.InputManifest.new(readYaml(file: args.inputManifest))
+
+    def DISTRIBUTION_JOB_NAME = args.jobName ?: "${JOB_NAME}"
+    def version = inputManifestObj.build.version
+
+    def DISTRIBUTION_PLATFORM = args.platform
+    def DISTRIBUTION_ARCHITECTURE = args.architecture
+    def distribution = args.distribution
+    def prefixPath = "${WORKSPACE}/download"
+    def previousBuildId = args.previousBuildId ?: "latest"
+    def DISTRIBUTION_BUILD_NUMBER
+
+    if (previousBuildId.equalsIgnoreCase("latest")) {
+        DISTRIBUTION_BUILD_NUMBER = sh(
+                script:  "curl -sL https://ci.opensearch.org/ci/dbc/${DISTRIBUTION_JOB_NAME}/${version}/index.json | jq -r \".latest\"",
+                returnStdout: true
+        ).trim()
+        //Once we have new index.json, URL will be changed to: https://ci.opensearch.org/ci/dbc/${DISTRIBUTION_JOB_NAME}/${version}/index/${platform}/${architecture}/${distribution}/index.json
+    } else {
+        DISTRIBUTION_BUILD_NUMBER = previousBuildId
+    }
+
+    def artifactPath = "${DISTRIBUTION_JOB_NAME}/${version}/${DISTRIBUTION_BUILD_NUMBER}/${DISTRIBUTION_PLATFORM}/${DISTRIBUTION_ARCHITECTURE}/${distribution}"
+
+    withCredentials([string(credentialsId: 'jenkins-artifact-bucket-name', variable: 'ARTIFACT_BUCKET_NAME')]) {
+        downloadFromS3(
+                assumedRoleName: "opensearch-bundle",
+                roleAccountNumberCred: "jenkins-aws-account-public",
+                downloadPath: "${artifactPath}/",
+                bucketName: "${ARTIFACT_BUCKET_NAME}",
+                localPath: "${prefixPath}",
+                force: true,
+        )
+    }
+    sh("mkdir -p ${distribution} && mv -v ${prefixPath}/${artifactPath} ${WORKSPACE}")
+    if (inputManifestObj.build.getFilename().equals("opensearch")) {
+        echo("Setting up Maven Local for OpenSearch build.")
+        sh("mkdir -p ~/.m2/repository/org/ && cp -r ${distribution}/builds/opensearch/maven/org/opensearch/ ~/.m2/repository/org/")
+    }
+
+}
