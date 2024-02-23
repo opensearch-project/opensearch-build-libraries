640773995aa25e41e8ac33226b15ee7ab13281c2
Divya Madala <divyaasm@amazon.com>
2024-02-23 13:22:25 -0800

    Bump lib version and add parameter description
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- vars/validateArtifacts.groovy
+++ vars/validateArtifacts.groovy
@@ -9,7 +9,21 @@
 
 /**
 Wrapper that runs validation.sh script with provided args.
-@param Map[<any>] <Required> - Any arguments that you want to be passed to validation.sh script. eg: version: 1.0.0 will be passed as --version 1.0.0
+[
+@param Map[version] <Required> - Version of the opensearch artifact that needs to be validated.
+@param Map[file_path] <Required> - Url paths to Opensearch or OpenSearch DashBoards artifacts.
+Note: These parameters are mutually exclusive. Provide either of 'version' or  'file_path' parameters, to proceed with the validation.
+]
+@param Map[distribution] <Optional> - Choose distribution type among [tar(default),rpm,yum,docker].
+@param Map[architecture] <Optional> - Select the architecture type among [x64(default) and arm64].
+@param Map[platform] <Optional> - The distribution platform.
+@param Map[projects] <Optional> - Specify the project type OpenSearch or Both(OpenSearch and OpenSearch-DashBoards).
+@param Map[docker_source] <Optional> - Specify the docker source from [DockerHub(default), ECR] to pull the docker image.
+@param Map[os_build_number]<Optional> - Specify Opensearch build number from opensearchstaging if required.
+@param Map[osd_build_number]<Optional> - Specify Opensearch-Dashboard build number from opensearchstaging if required.
+@param Map[artifact_type] <Optional> - Select the artifact type among [staging and production].
+@param Map[allow_http] <Optional> - Supports validation of artifacts in which security plugin is absent.architecture.
+@param Map[docker_args] <Optional> - Select either of [using-staging-artifact-only', 'validate-digest-only] for docker validation.
 */
 void call(Map args = [:]) {
     if (!fileExists("$WORKSPACE/validation.sh")) {
