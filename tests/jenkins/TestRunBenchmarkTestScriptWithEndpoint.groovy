7937d8e1bfe6f1df0710f542396349cdea442063
Divya Madala <divyaasm@amazon.com>
2024-03-27 08:44:25 -0700

    Add changes to benchmark groovy library to support test runs against an endpoint
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

---
+++ tests/jenkins/TestRunBenchmarkTestScriptWithEndpoint.groovy
@@ -0,0 +1,73 @@
+
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+import jenkins.tests.BuildPipelineTest
+import org.junit.Before
+import org.junit.Test
+
+import static com.lesfurets.jenkins.unit.MethodCall.callArgsToString
+import static org.hamcrest.CoreMatchers.equalTo
+import static org.hamcrest.CoreMatchers.hasItem
+import static org.hamcrest.MatcherAssert.assertThat
+
+class TestRunBenchmarkTestScriptWithEndpoint extends BuildPipelineTest {
+
+    @Before
+    void setUp() {
+        this.registerLibTester(new RunBenchmarkTestEndpointLibTester(
+                'opensearch-ABCxdfdfhyfk.com',
+                'false',
+                'nyc_taxis',
+                'true',
+                '',
+                '',
+                '',
+                '',
+                '',
+                ''
+        ))
+        super.setUp()
+    }
+
+    @Test
+    public void testRunBenchmarkTestScript_PipelineSingleNode() {
+        super.testPipeline("tests/jenkins/jobs/BenchmarkTestWithEndpoint_Jenkinsfile")
+    }
+
+    @Test
+    void testRunBenchmarkTestScript_verifyScriptExecutionsNoManifest() {
+        runScript("tests/jenkins/jobs/BenchmarkTestWithEndpoint_Jenkinsfile")
+
+        def testScriptCommands = getCommandExecutions('sh', './test.sh').findAll {
+            shCommand -> shCommand.contains('./test.sh')
+        }
+
+        assertThat(testScriptCommands.size(), equalTo(1))
+        assertThat(testScriptCommands, hasItem(
+                "./test.sh benchmark-test    --cluster-endpoint opensearch-ABCxdfdfhyfk.com  --workload nyc_taxis --benchmark-config /tmp/workspace/benchmark.ini --user-tag true,security-enabled:true"
+        ))
+    }
+
+    def getCommandExecutions(methodName, command) {
+        def shCommands = helper.callStack.findAll {
+            call ->
+                call.methodName == methodName
+        }.
+                collect {
+                    call ->
+                        callArgsToString(call)
+                }.findAll {
+            shCommand ->
+                shCommand.contains(command)
+        }
+
+        return shCommands
+    }
+}
