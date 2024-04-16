111fc9579eea6af2191d13ad3b5c2b743fc41b33
Sayali Gaikawad <61760125+gaiksaya@users.noreply.github.com>
2024-04-16 12:23:44 -0700

    Fix setting ruby version (#417)
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- vars/publishToRubyGems.groovy
+++ vars/publishToRubyGems.groovy
@@ -23,8 +23,8 @@ void call(Map args = [:]) {
     String rubyVersion = args.rubyVersion ?: '2.6.0'
 
     sh """#!/bin/bash
-        gem cert --add ${certPath}
         source ~/.rvm/scripts/rvm && rvm use ${rubyVersion} && ruby --version
+        gem cert --add ${certPath}
         cd ${releaseArtifactsDir} && gemNameWithVersion=\$(ls *.gem)
         gem install \$gemNameWithVersion
         gemName=\$(echo \$gemNameWithVersion | sed -E 's/(-[0-9.]+-*[a-z]*.gem\$)//g')
