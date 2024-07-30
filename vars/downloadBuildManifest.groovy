7ddabf2ced8ea07d2da59857ec6fc9a49c08a27f
Prudhvi Godithi <pgodithi@amazon.com>
2024-07-30 15:03:41 -0700

    [Bug]: Library `downloadBuildManifest` error `curl: (23) Failed writing body (0 != 4804)` (#468)
    Signed-off-by: Prudhvi Godithi <pgodithi@amazon.com>

--- vars/downloadBuildManifest.groovy
+++ vars/downloadBuildManifest.groovy
@@ -8,8 +8,9 @@
  */
 def call(Map args = [:]) {
     def lib = library(identifier: "jenkins@main", retriever: legacySCM(scm))
-
-    sh "curl -sSL ${args.url} --output ${args.path}"
+    sh "mkdir -p \$(dirname \"${args.path}\")"
+    sh "touch ${args.path}"
+    sh "curl -sSL --retry 5 ${args.url} --output ${args.path}"
     def buildManifestObj = lib.jenkins.BuildManifest.new(readYaml(file: args.path))
     return buildManifestObj
 }
