520b348353c9421d043e31a15ae44e879c83ff59
Prudhvi Godithi <pgodithi@amazon.com>
2024-07-31 10:13:03 -0700

    Manual backport of PR 468
    Signed-off-by: Prudhvi Godithi <pgodithi@amazon.com>

--- vars/downloadBuildManifest.groovy
+++ vars/downloadBuildManifest.groovy
@@ -7,7 +7,7 @@
  * compatible open source license.
  */
 def call(Map args = [:]) {
-    def lib = library(identifier: "jenkins@main", retriever: legacySCM(scm))
+    def lib = library(identifier: "jenkins@6.7.2", retriever: legacySCM(scm))
     sh "mkdir -p \$(dirname \"${args.path}\")"
     sh "touch ${args.path}"
     sh "curl -sSL --retry 5 ${args.url} --output ${args.path}"
