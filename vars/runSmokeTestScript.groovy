7f69162e042e4300604b45759180483a1d6d1005
Zelin Hao <zelinhao@amazon.com>
2025-01-13 16:03:30 -0800

    Add argument to switch to non root user for running smoke test script (#572)
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- vars/runSmokeTestScript.groovy
+++ vars/runSmokeTestScript.groovy
@@ -13,6 +13,7 @@
  * @param args.testManifest <required> Test manifest file location
  * @param args.buildManifest <required> Build manifest file location
  * @param args.buildId <required> Build ID of the distribution artifacts
+ * @param args.switchUserNonRoot <Required> Switch to non root user if true.
  * @param args.jobName <optional> Job name that triggered the workflow. 'distribution-build-opensearh' by default.
 */
 void call(Map args = [:]) {
@@ -22,14 +23,26 @@ void call(Map args = [:]) {
     String artifactRootUrl = buildManifest.getArtifactRootUrl(jobName, args.buildId)
     echo "Artifact root URL: ${artifactRootUrl}"
 
+    String switchUser = args.switchUserNonRoot ?: 'false'
+    if (! (switchUser == 'true' || switchUser == 'false')) {
+        echo "args.switchUserNonRoot is not valid. Exit the program."
+        System.exit(1)
+    }
+    echo "Switch User to Non-Root (uid=1000): ${switchUser}"
+
+    String switchCommandStart = switchUser == 'true' ? "su `id -un 1000` -c \"" : ""
+    String switchCommandEnd = switchUser == 'true' ? '"' : ''
+
     String paths = "opensearch=${artifactRootUrl}"
     echo "Paths: ${paths}"
 
     sh([
-        './test.sh',
-        'smoke-test',
-        "${args.testManifest}",
-        "--test-run-id ${env.BUILD_NUMBER}",
-        "--paths ${paths}",
+            switchCommandStart,
+            './test.sh',
+            'smoke-test',
+            "${args.testManifest}",
+            "--test-run-id ${env.BUILD_NUMBER}",
+            "--paths ${paths}",
+            switchCommandEnd,
     ].join(' '))
 }
