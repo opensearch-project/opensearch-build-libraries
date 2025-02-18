b67efcbfc320a057e9932ca3de66584532af6db3
Sayali Gaikawad <gaiksaya@amazon.com>
2025-02-18 14:38:56 -0800

    Add library to check, request and assign component release owners
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

---
+++ src/jenkins/ComponentRepoData.groovy
@@ -0,0 +1,81 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package jenkins
+
+import groovy.json.JsonOutput
+import utils.OpenSearchMetricsQuery
+
+class ComponentRepoData {
+    String metricsUrl
+    String awsAccessKey
+    String awsSecretKey
+    String awsSessionToken
+    String version
+    String indexName
+    def script
+    def openSearchMetricsQuery
+
+    ComponentRepoData(String metricsUrl, String awsAccessKey, String awsSecretKey, String awsSessionToken, String version, String indexName, def script) {
+        this.metricsUrl = metricsUrl
+        this.awsAccessKey = awsAccessKey
+        this.awsSecretKey = awsSecretKey
+        this.awsSessionToken = awsSessionToken
+        this.version = version
+        this.indexName = indexName
+        this.script = script
+        this.openSearchMetricsQuery = new OpenSearchMetricsQuery(metricsUrl, awsAccessKey, awsSecretKey, awsSessionToken, indexName, script)
+    }
+
+    String getMaintainersQuery(String repository, Boolean inactive=false) {
+        def queryMap = [
+                size   : 100,
+                _source: "github_login",
+                collapse: [
+                        field: "github_login.keyword"
+                ],
+                query  : [
+                        bool: [
+                                filter: [
+                                        [
+                                                match_phrase: [
+                                                        "repository.keyword": "${repository}"
+                                                ]
+                                        ],
+                                        [
+                                                match_phrase: [
+                                                        "inactive": "${inactive}"
+                                                ]
+                                        ]
+                                ]
+                        ]
+                ],
+                sort   : [
+                        [
+                                current_date: [
+                                        order: "desc"
+                                ]
+                        ]
+                ]
+        ]
+        String query = JsonOutput.toJson(queryMap)
+        return query.replace('"', '\\"')
+    }
+
+    ArrayList getMaintainers(String repository, Boolean inactive=false) {
+        try {
+            def jsonResponse = this.openSearchMetricsQuery.fetchMetrics(getMaintainersQuery(repository, inactive))
+            def maintainers = jsonResponse.hits.hits.collect { it._source.github_login }
+            return maintainers
+        } catch (Exception e) {
+            this.script.println("Error fetching the maintainers for ${repository}: ${e.message}")
+            return null
+        }
+    }
+}
