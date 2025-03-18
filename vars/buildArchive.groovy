e5d83b93f0ce53c627c6b0ffc2abf4fe1528cf88
Peter Zhu <zhujiaxi@amazon.com>
2025-03-18 17:22:47 -0400

    Bump version to 8.3.4 for 8.3 branch (#630)
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- vars/buildArchive.groovy
+++ vars/buildArchive.groovy
@@ -7,7 +7,7 @@
  * compatible open source license.
  */
 void call(Map args = [:]) {
-    def lib = library(identifier: 'jenkins@8.3.3', retriever: legacySCM(scm))
+    def lib = library(identifier: 'jenkins@8.3.4', retriever: legacySCM(scm))
     buildManifest(args)
 
     String stashName = "${args.stashName}"
