f32e5bc8b2e6d5214b4ffa34c18c68c263eb4082
Sayali Gaikawad <gaiksaya@amazon.com>
2025-03-21 16:02:14 -0700

    Add mock for random
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- tests/utils/TestTemplateProcessor.groovy
+++ tests/utils/TestTemplateProcessor.groovy
@@ -14,10 +14,7 @@ import org.junit.Before
 import org.junit.Test
 import utils.TemplateProcessor
 import static org.junit.Assert.assertEquals
-<<<<<<< HEAD
 import static org.junit.Assert.assertThrows
-=======
->>>>>>> 4e96a439 (Add library to check the code-coverage status)
 
 class TestTemplateProcessor {
     def script
@@ -49,44 +46,15 @@ class TestTemplateProcessor {
 
     @Test
     void testProcessor() {
-<<<<<<< HEAD
         Random.metaClass.nextInt = { int max -> 1 }
-=======
-
->>>>>>> 4e96a439 (Add library to check the code-coverage status)
         def bindings = [
                 BRANCH: 'main',
                 VERSION: '3.0'
         ]
         def templateProcessor = new TemplateProcessor(script)
         def result  = templateProcessor.process("release/missing-code-coverage.md", bindings, '/tmp/workspace')
-<<<<<<< HEAD
-        assertEquals (result, "/tmp/workspace/BBBBBBBBBB.md")
-        assertEquals(writtenFiles["Content"].toString(),"[This is a test template checking values for main and 3.0]" )
-    }
-
-    @Test
-    void testProcessorException() {
-        def bindings = [
-                BRANCH: '',
-                VERSION: '3.0'
-        ]
-        script.libraryResource = { path ->
-            // Return a sample template content for testing
-            throw new IOException("Resource not found, ${path}")
-        }
-        def templateProcessor = new TemplateProcessor(script)
-        try {
-            templateProcessor.process("/tmp", bindings, '/tmp/workspace')
-            fail("Expected an exception to be thrown")
-        } catch (Exception e) {
-            assertEquals("Failed to process template: Resource not found, /tmp", e.getMessage())
-        }
-    }
-=======
         assert result.startsWith("/tmp/workspace/")
         assertEquals(writtenFiles["Content"].toString(),"[This is a test template checking values for main and 3.0]" )
     }
->>>>>>> 4e96a439 (Add library to check the code-coverage status)
 }
 
