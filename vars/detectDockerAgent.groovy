71650acea3771a54750c156d0afed6d17983428c
Peter Zhu <zhujiaxi@amazon.com>
2025-06-02 11:33:59 -0400

    Add new defaults to docker image
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- vars/detectDockerAgent.groovy
+++ vars/detectDockerAgent.groovy
@@ -26,7 +26,7 @@ Map call(Map args = [:]) {
     def schemaVersion = inputManifest.schemaVersion ?: "None"
     echo "InputManifest SchemaVersion: ${schemaVersion}"
 
-    def dockerImage = 'opensearchstaging/ci-runner:ci-runner-centos7-v1'
+    def dockerImage = 'opensearchstaging/ci-runner:ci-runner-al2-opensearch-build-v1'
     def dockerArgs = '-e JAVA_HOME=/opt/java/openjdk-21'  // Using default javaVersion as openjdk-21
     def javaVersion = 'openjdk-21'
 
