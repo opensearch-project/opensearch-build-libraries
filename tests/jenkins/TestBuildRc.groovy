f73bbc75d6574581601667e3c871fc503cd9a00f
Sayali Gaikawad <gaiksaya@amazon.com>
2025-09-25 13:24:15 -0700

    Add a library to build RC with required params (#765)

---
+++ tests/jenkins/TestBuildRc.groovy
@@ -0,0 +1,212 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+package jenkins.tests
+
+import jenkins.tests.BuildPipelineTest
+import org.junit.Before
+import org.junit.Test
+import static com.lesfurets.jenkins.unit.MethodCall.callArgsToString
+import static org.hamcrest.CoreMatchers.containsString
+import static org.hamcrest.CoreMatchers.hasItem
+import static org.hamcrest.MatcherAssert.assertThat
+
+class TestBuildRc extends BuildPipelineTest {
+
+    @Override
+    @Before
+    void setUp() {
+        super.setUp()
+        helper.registerAllowedMethod("withSecrets", [Map])
+        binding.setVariable('env', [
+                'METRICS_HOST_URL'     : 'sample.url',
+                'AWS_ACCESS_KEY_ID'    : 'abc',
+                'AWS_SECRET_ACCESS_KEY': 'xyz',
+                'AWS_SESSION_TOKEN'    : 'sampleToken'
+        ])
+        helper.registerAllowedMethod("withSecrets", [Map, Closure], { args, closure ->
+            closure.delegate = delegate
+            return helper.callClosure(closure)
+        })
+        binding.setVariable('METRICS_HOST_ACCOUNT', "METRICS_HOST_ACCOUNT")
+        helper.registerAllowedMethod('withAWS', [Map, Closure], { args, closure ->
+            closure.delegate = delegate
+            return helper.callClosure(closure)
+        })
+        def osLatestRcResponse = '''
+                    {
+                      "took": 16,
+                      "timed_out": false,
+                      "_shards": {
+                        "total": 40,
+                        "successful": 40,
+                        "skipped": 0,
+                        "failed": 0
+                      },
+                      "hits": {
+                        "total": {
+                          "value": 11,
+                          "relation": "eq"
+                        },
+                        "max_score": null,
+                        "hits": [
+                          {
+                            "_index": "opensearch-distribution-build-results-02-2025",
+                            "_id": "xAsw15QB2OP_jOaCGo2o",
+                            "_score": null,
+                            "_source": {
+                              "rc_number": 5
+                            },
+                            "sort": [
+                              10787,
+                              5
+                            ]
+                          }
+                        ]
+                      }
+                    }
+                    '''
+
+        def osdLatestRcResponse = '''
+                    {
+                      "took": 15,
+                      "timed_out": false,
+                      "_shards": {
+                        "total": 40,
+                        "successful": 40,
+                        "skipped": 0,
+                        "failed": 0
+                      },
+                      "hits": {
+                        "total": {
+                          "value": 14,
+                          "relation": "eq"
+                        },
+                        "max_score": null,
+                        "hits": [
+                          {
+                            "_index": "opensearch-distribution-build-results-02-2025",
+                            "_id": "5MSv3pQBhoDV_8ni75zx",
+                            "_score": null,
+                            "_source": {
+                              "rc_number": 5
+                            },
+                            "sort": [
+                              8260,
+                              5
+                            ]
+                          }
+                        ]
+                      }
+                    }
+                    '''
+
+        helper.addShMock("""\n            set -e\n            set +x\n            curl -s -XGET \"sample.url/opensearch-distribution-build-results/_search\" --aws-sigv4 \"aws:amz:us-east-1:es\" --user \"abc:xyz\" -H \"x-amz-security-token:sampleToken\" -H 'Content-Type: application/json' -d \"{\\"_source\\":\\"rc_number\\",\\"sort\\":[{\\"distribution_build_number\\":{\\"order\\":\\"desc\\"},\\"rc_number\\":{\\"order\\":\\"desc\\"}}],\\"size\\":1,\\"query\\":{\\"bool\\":{\\"filter\\":[{\\"match_phrase\\":{\\"component\\":\\"OpenSearch\\"}},{\\"match_phrase\\":{\\"rc\\":\\"true\\"}},{\\"match_phrase\\":{\\"version\\":\\"2.19.0\\"}}]}}}\" | jq '.'\n        """) { script ->
+            return [stdout: osLatestRcResponse, exitValue: 0]
+        }
+        helper.addShMock("""\n            set -e\n            set +x\n            curl -s -XGET \"sample.url/opensearch-distribution-build-results/_search\" --aws-sigv4 \"aws:amz:us-east-1:es\" --user \"abc:xyz\" -H \"x-amz-security-token:sampleToken\" -H 'Content-Type: application/json' -d \"{\\"_source\\":\\"rc_number\\",\\"sort\\":[{\\"distribution_build_number\\":{\\"order\\":\\"desc\\"},\\"rc_number\\":{\\"order\\":\\"desc\\"}}],\\"size\\":1,\\"query\\":{\\"bool\\":{\\"filter\\":[{\\"match_phrase\\":{\\"component\\":\\"OpenSearch-Dashboards\\"}},{\\"match_phrase\\":{\\"rc\\":\\"true\\"}},{\\"match_phrase\\":{\\"version\\":\\"2.19.0\\"}}]}}}\" | jq '.'\n        """) { script ->
+            return [stdout: osdLatestRcResponse, exitValue: 0]
+        }
+    }
+
+    @Test
+    void testBuildingBothRC() {
+        addParam('PRODUCT', 'both')
+        this.registerLibTester(new BuildRcLibTester('2.19.0'))
+        super.testPipeline('tests/jenkins/jobs/BuildRc.jenkinsFile')
+        def callStack = helper.getCallStack()
+        assertCallStack().contains('BuildRc.buildRC({version=2.19.0, product=both})')
+        assertCallStack().contains('buildRC.string({name=INPUT_MANIFEST, value=2.19.0/opensearch-2.19.0.yml})')
+        assertCallStack().contains('buildRC.string({name=TEST_MANIFEST, value=2.19.0/opensearch-2.19.0-test.yml})')
+        assertCallStack().contains('buildRC.string({name=BUILD_PLATFORM, value=linux windows})')
+        assertCallStack().contains('buildRC.string({name=BUILD_DISTRIBUTION, value=tar rpm deb zip})')
+        assertCallStack().contains('buildRC.string({name=TEST_PLATFORM, value=linux windows})')
+        assertCallStack().contains('buildRC.string({name=TEST_DISTRIBUTION, value=tar rpm deb zip})')
+        assertCallStack().contains('buildRC.string({name=RC_NUMBER, value=6})')
+        assertCallStack().contains('buildRC.string({name=BUILD_DOCKER, value=build_docker_with_build_number_tag})')
+        assertCallStack().contains('buildRC.booleanParam({name=CONTINUE_ON_ERROR, value=false})')
+        assertCallStack().contains('buildRC.booleanParam({name=UPDATE_GITHUB_ISSUE, value=true})')
+        assertCallStack().contains('buildRC.build({job=distribution-build-opensearch, parameters=[null, null, null, null, null, null, null, null, null, null], wait=true, propagate=true})')
+        assertCallStack().contains('buildRC.string({name=INPUT_MANIFEST, value=2.19.0/opensearch-dashboards-2.19.0.yml})')
+        assertCallStack().contains('buildRC.string({name=TEST_MANIFEST, value=2.19.0/opensearch-dashboards-2.19.0-test.yml})')
+        assertCallStack().contains('buildRC.string({name=BUILD_PLATFORM, value=linux windows})')
+        assertCallStack().contains('buildRC.string({name=BUILD_DISTRIBUTION, value=tar rpm deb zip})')
+        assertCallStack().contains('buildRC.string({name=TEST_PLATFORM, value=linux windows})')
+        assertCallStack().contains('buildRC.string({name=TEST_DISTRIBUTION, value=tar rpm deb zip})')
+        assertCallStack().contains('buildRC.string({name=RC_NUMBER, value=6})')
+        assertCallStack().contains('buildRC.string({name=BUILD_DOCKER, value=build_docker_with_build_number_tag})')
+        assertCallStack().contains('buildRC.booleanParam({name=CONTINUE_ON_ERROR, value=false})')
+        assertCallStack().contains('buildRC.booleanParam({name=UPDATE_GITHUB_ISSUE, value=true})')
+        assertCallStack().contains('buildRC.build({job=distribution-build-opensearch-dashboards, parameters=[null, null, null, null, null, null, null, null, null, null], wait=true, propagate=true})')
+        assertThat(getCommands('echo', 'Current'), hasItem('Current RC numbers: OpenSearch - 5, OpenSearch-Dashboards - 5'))
+        assertThat(getCommands('echo', 'Triggering'), hasItem('Triggering both OpenSearch and OpenSearch-Dashboards builds with RC numbers: 6, 6 respectively'))
+    }
+
+    @Test
+    void testBuildingOpenSearchOnlyRC() {
+        addParam('PRODUCT', 'opensearch')
+        this.registerLibTester(new BuildRcLibTester('2.19.0', 'opensearch'))
+        runScript('tests/jenkins/jobs/BuildRc.jenkinsFile')
+        def callStack = helper.getCallStack()
+        assertThat(getCommands('echo', 'Current'), hasItem('Current RC numbers: OpenSearch - 5, OpenSearch-Dashboards - 5'))
+        assertThat(getCommands('echo', 'triggering'), hasItem('Only triggering OpenSearch build with RC number: 6'))
+        assertCallStack().contains('BuildRc.buildRC({version=2.19.0, product=opensearch})')
+        assertCallStack().contains('buildRC.string({name=INPUT_MANIFEST, value=2.19.0/opensearch-2.19.0.yml})')
+        assertCallStack().contains('buildRC.string({name=TEST_MANIFEST, value=2.19.0/opensearch-2.19.0-test.yml})')
+        assertCallStack().contains('buildRC.string({name=BUILD_PLATFORM, value=linux windows})')
+        assertCallStack().contains('buildRC.string({name=BUILD_DISTRIBUTION, value=tar rpm deb zip})')
+        assertCallStack().contains('buildRC.string({name=TEST_PLATFORM, value=linux windows})')
+        assertCallStack().contains('buildRC.string({name=TEST_DISTRIBUTION, value=tar rpm deb zip})')
+        assertCallStack().contains('buildRC.string({name=RC_NUMBER, value=6})')
+        assertCallStack().contains('buildRC.string({name=BUILD_DOCKER, value=build_docker_with_build_number_tag})')
+        assertCallStack().contains('buildRC.booleanParam({name=CONTINUE_ON_ERROR, value=false})')
+        assertCallStack().contains('buildRC.booleanParam({name=UPDATE_GITHUB_ISSUE, value=true})')
+        assertCallStack().contains('buildRC.build({job=distribution-build-opensearch, parameters=[null, null, null, null, null, null, null, null, null, null], wait=true, propagate=true})')
+    }
+
+    @Test
+    void testBuildingOsdOnlyRC() {
+        addParam('PRODUCT', 'opensearch-dashboards')
+        this.registerLibTester(new BuildRcLibTester('2.19.0', 'opensearch-dashboards'))
+        runScript('tests/jenkins/jobs/BuildRc.jenkinsFile')
+        def callStack = helper.getCallStack()
+        assertThat(getCommands('echo', 'Current'), hasItem('Current RC numbers: OpenSearch - 5, OpenSearch-Dashboards - 5'))
+        assertThat(getCommands('echo', 'triggering'), hasItem('Only triggering OpenSearch-Dashboards build with RC number: 6'))
+        assertCallStack().contains('buildRC.string({name=INPUT_MANIFEST, value=2.19.0/opensearch-dashboards-2.19.0.yml})')
+        assertCallStack().contains('buildRC.string({name=TEST_MANIFEST, value=2.19.0/opensearch-dashboards-2.19.0-test.yml})')
+        assertCallStack().contains('buildRC.string({name=BUILD_PLATFORM, value=linux windows})')
+        assertCallStack().contains('buildRC.string({name=BUILD_DISTRIBUTION, value=tar rpm deb zip})')
+        assertCallStack().contains('buildRC.string({name=TEST_PLATFORM, value=linux windows})')
+        assertCallStack().contains('buildRC.string({name=TEST_DISTRIBUTION, value=tar rpm deb zip})')
+        assertCallStack().contains('buildRC.string({name=RC_NUMBER, value=6})')
+        assertCallStack().contains('buildRC.string({name=BUILD_DOCKER, value=build_docker_with_build_number_tag})')
+        assertCallStack().contains('buildRC.booleanParam({name=CONTINUE_ON_ERROR, value=false})')
+        assertCallStack().contains('buildRC.booleanParam({name=UPDATE_GITHUB_ISSUE, value=true})')
+        assertCallStack().contains('buildRC.build({job=distribution-build-opensearch-dashboards, parameters=[null, null, null, null, null, null, null, null, null, null], wait=true, propagate=true})')
+    }
+
+    @Test
+    void testWrongProduct() {
+        addParam('PRODUCT', 'opensearch-dashboard')
+        this.registerLibTester(new BuildRcLibTester('2.19.0', 'opensearch-dashboard'))
+        runScript('tests/jenkins/jobs/BuildRc.jenkinsFile')
+        assertThat(getCommands('error', ''), hasItem("Invalid product 'opensearch-dashboard'. Valid values: opensearch, opensearch-dashboards, both"))
+        assertJobStatusFailure()
+    }
+
+    def getCommands(method, text) {
+        def shCommands = helper.callStack.findAll { call ->
+            call.methodName == method
+        }.collect { call ->
+            callArgsToString(call)
+        }.findAll { command ->
+            command.contains(text)
+        }
+        return shCommands
+    }
+}
---
+++ vars/buildRC.groovy
@@ -0,0 +1,122 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+import jenkins.ReleaseCandidateStatus
+
+/** Library to build release candidate for OpenSearch and OpenSearch-Dashboards
+ *  The library will fetch the latest RC number from metrics and increment it by 1.
+ *  For manual RC number, please use the distribution-build-opensearch or distribution-build-opensearch-dashboards job directly
+ *  @param Map args = [:] args A map of the following parameters
+ *  @param args.version <required> - Release version along with qualifier eg:3.0.0-alpha1.
+ *  @param args.product <optional> - Product name. eg: opensearch and/or opensearch-dashboards. Defaults to both
+ **/
+
+void call(Map args = [:]) {
+    def secret_metrics_cluster = [
+        [envVar: 'METRICS_HOST_ACCOUNT', secretRef: 'op://opensearch-infra-secrets/aws-accounts/jenkins-health-metrics-account-number'],
+        [envVar: 'METRICS_HOST_URL', secretRef: 'op://opensearch-infra-secrets/metrics-cluster/jenkins-health-metrics-cluster-endpoint']
+    ]
+    validateParameters(args)
+    def buildIndexName = 'opensearch-distribution-build-results'
+    def versionTokenize = args.version.tokenize('-')
+    def version = versionTokenize[0]
+    def qualifier = versionTokenize[1] ?: "None"
+    def opensearchRcNumber
+    def opensearchDashboardsRcNumber
+
+    withSecrets(secrets: secret_metrics_cluster){
+        withAWS(role: 'OpenSearchJenkinsAccessRole', roleAccount: "${METRICS_HOST_ACCOUNT}", duration: 900, roleSessionName: 'jenkins-session') {
+            def metricsUrl = env.METRICS_HOST_URL
+            def awsAccessKey = env.AWS_ACCESS_KEY_ID
+            def awsSecretKey = env.AWS_SECRET_ACCESS_KEY
+            def awsSessionToken = env.AWS_SESSION_TOKEN
+
+            ReleaseCandidateStatus releaseCandidateStatus = new ReleaseCandidateStatus(metricsUrl, awsAccessKey, awsSecretKey, awsSessionToken, buildIndexName, version, qualifier, this)
+
+            opensearchRcNumber = releaseCandidateStatus.getLatestRcNumber('OpenSearch')
+            opensearchDashboardsRcNumber = releaseCandidateStatus.getLatestRcNumber('OpenSearch-Dashboards')
+        }
+    }
+
+    if (isNullOrEmpty(opensearchRcNumber.toString()) || isNullOrEmpty(opensearchDashboardsRcNumber.toString())) {
+        error("Unable to fetch latest RC number from metrics")
+    }
+    else {
+        echo("Current RC numbers: OpenSearch - ${opensearchRcNumber}, OpenSearch-Dashboards - ${opensearchDashboardsRcNumber}")
+        opensearchRcNumber = (opensearchRcNumber as Integer) + 1
+        opensearchDashboardsRcNumber = (opensearchDashboardsRcNumber as Integer) + 1
+        switch (args.product) {
+            case ("opensearch"):
+                echo("Only triggering OpenSearch build with RC number: ${opensearchRcNumber}")
+                triggerBuildWorkflow(args.version, 'opensearch', opensearchRcNumber.toString())
+                break
+            case ("opensearch-dashboards"):
+                echo("Only triggering OpenSearch-Dashboards build with RC number: ${opensearchDashboardsRcNumber}")
+                triggerBuildWorkflow(args.version, 'opensearch-dashboards', opensearchDashboardsRcNumber.toString())
+                break
+            case ("both"):
+                echo("Triggering both OpenSearch and OpenSearch-Dashboards builds with RC numbers: ${opensearchRcNumber}, ${opensearchDashboardsRcNumber} respectively")
+                parallel(
+                    opensearch: {
+                        triggerBuildWorkflow(args.version, 'opensearch', opensearchRcNumber.toString())
+                    },
+                    opensearchDashboards: {
+                        triggerBuildWorkflow(args.version, 'opensearch-dashboards', opensearchDashboardsRcNumber.toString())
+                    }
+                )
+                break
+        }
+    }
+}
+
+/**
+ * Trigger build workflow for a specific product and required parameters for RC
+ */
+def triggerBuildWorkflow(String version, String product, String rcNumber) {
+        build job: "distribution-build-${product}", 
+        parameters: [
+            string(name: 'INPUT_MANIFEST', value: "${version}/${product}-${version}.yml"),
+            string(name: 'TEST_MANIFEST', value: "${version}/${product}-${version}-test.yml"),
+            string(name: 'BUILD_PLATFORM', value: "linux windows"),
+            string(name: 'BUILD_DISTRIBUTION', value: "tar rpm deb zip"),
+            string(name: 'TEST_PLATFORM', value: "linux windows"),
+            string(name: 'TEST_DISTRIBUTION', value: "tar rpm deb zip"),
+            string(name: 'RC_NUMBER', value: "${rcNumber}"),
+            string(name: 'BUILD_DOCKER', value: "build_docker_with_build_number_tag"),
+            booleanParam(name: 'CONTINUE_ON_ERROR', value: false),
+            booleanParam(name: 'UPDATE_GITHUB_ISSUE', value: true),
+        ], 
+        wait: true,
+        propagate: true
+}
+
+/**
+ * Validates input parameters
+ */
+private void validateParameters(Map args) {
+    if (!args.version) {
+        error "Version parameter is required."
+    }
+    if (isNullOrEmpty(args.product)) {
+        args.product = 'both'
+    }
+    if (args.product) {
+        List<String> validProducts = ['opensearch', 'opensearch-dashboards', 'both']
+        if (!validProducts.contains(args.product.toString())) {
+            error "Invalid product '${args.product}'. Valid values: ${validProducts.join(', ')}"
+        }
+    }
+}
+
+/**
+ * Check if a string is null or empty
+ */
+private boolean isNullOrEmpty(String str) {
+    return (str == 'Null' || str == null || str.allWhitespace || str.isEmpty()) || str == "None"
+}
