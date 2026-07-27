ee25c0e44e3ecf79e8ff509c79770d627d6d6788
opensearch-ci <83309141+opensearch-ci-bot@users.noreply.github.com>
2026-07-27 16:56:53 -0400

    [Backport 13.x] Refactor checkReleaseNotes to use metrics data and change return type (#874)
    Co-authored-by: Sayali Gaikawad <gaiksaya@amazon.com>
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>
    Signed-off-by: opensearch-ci-bot <opensearch-infra@amazon.com>

--- tests/jenkins/lib-testers/CheckReleaseNotesLibTester.groovy
+++ tests/jenkins/lib-testers/CheckReleaseNotesLibTester.groovy
@@ -12,13 +12,11 @@ import static org.hamcrest.CoreMatchers.notNullValue
 import static org.hamcrest.MatcherAssert.assertThat
 
 class CheckReleaseNotesLibTester extends LibFunctionTester{
-    private String version
-    private String dataTable
+    private List inputManifest
     private String action = 'check'
 
-    public CheckReleaseNotesLibTester(version, dataTable, action){
-        this.version = version
-        this.dataTable = dataTable
+    public CheckReleaseNotesLibTester(inputManifest, action){
+        this.inputManifest = inputManifest
         this.action = action
     }
 
@@ -29,15 +27,13 @@ class CheckReleaseNotesLibTester extends LibFunctionTester{
 
     @Override
     void parameterInvariantsAssertions(Object call) {
-        assertThat(call.args.version.first(), notNullValue())
-        assertThat(call.args.dataTable.first(), notNullValue())
+        assertThat(call.args.inputManifest.first(), notNullValue())
         assertThat(call.args.action.first(), notNullValue())
     }
 
     @Override
     boolean expectedParametersMatcher(Object call) {
-        return call.args.version.first().equals(this.version)
-                && call.args.dataTable.first().equals(this.dataTable)
+        return call.args.inputManifest.first().equals(this.inputManifest)
                 && call.args.action.first().toString().equals(this.action)
     }
 
