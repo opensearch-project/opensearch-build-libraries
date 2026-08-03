50173f8800a3fbaf2ba69e735a3ca55f4eadd5c4
opensearch-ci <83309141+opensearch-ci-bot@users.noreply.github.com>
2026-08-03 16:03:07 -0400

    [Backport 13.x] Update ECR mirror on the promoteContainer.groovy (#886)
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    Signed-off-by: opensearch-ci-bot <opensearch-infra@amazon.com>
    Co-authored-by: Peter Zhu <zhujiaxi@amazon.com>

--- vars/promoteContainer.groovy
+++ vars/promoteContainer.groovy
@@ -31,7 +31,7 @@ void call(Map args = [:]) {
 
     def sourceReg = (imageProduct == 'data-prepper') ? "${DATA_PREPPER_STAGING_CONTAINER_REPOSITORY}" : "opensearchstaging"
     def dockerProduction = "opensearchproject"
-    def ecrProduction = "public.ecr.aws/opensearchproject"
+    def ecrProduction = "public.ecr.aws/opensearchorg/opensearchproject"
 
     //Promoting docker images
     if (dockerPromote.toBoolean()) {
